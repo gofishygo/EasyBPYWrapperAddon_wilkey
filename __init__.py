40e5cfaec2381518ea997a775aa621836f814f2b
CGArtPython <65920683+CGArtPython@users.noreply.github.com>
2023-09-17 00:17:17 -0700

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@ import os
 import sys
 
 ADDON_FOLDER_PATH = os.path.dirname(__file__)
-VERSION = (0, 1, 8)
+VERSION = (0, 2, 0)
 MODULE_NAME = "easybpy"
 ADDON_NAME = (
     f"EasyBPY v{VERSION[0]}.{VERSION[1]}.{VERSION[2]}"
@@ -11,11 +11,11 @@ ADDON_NAME = (
 bl_info = {
     "name": "EasyBPY v0.1.8",
     "author": "Curtis Holt",
-    "version": (0, 1, 8),
+    "version": (0, 2, 0),
     "blender": (2, 83, 0),
     "description": "A module to simplify the use of the Blender Python API.",
     "category": "Development",
-    "doc_url": "",
+    "doc_url": "https://curtisholt.online/easybpy",
 }
 
 
